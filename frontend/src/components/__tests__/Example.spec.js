0c8e79c960fa044d37640ea1659dad4649bc2db9
star <itcoder007@126.com>
2025-06-22 04:32:36 +0800

    完成story1.2

---
+++ frontend/src/components/__tests__/Example.spec.js
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+
+describe('Example Test', () => {
+  it('should pass', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
