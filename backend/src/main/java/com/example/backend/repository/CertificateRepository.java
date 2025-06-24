540583b26dfb3a62954b10c583956da0ca01eb13
star <itcoder007@126.com>
2025-06-25 05:22:38 +0800

    初始化

--- backend/src/main/java/com/example/backend/repository/CertificateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.backend.domain.Certificate;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CertificateRepository extends BaseMapper<Certificate> {
-}
